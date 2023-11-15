dbed3e5682afb292b0b323969a46154a045018b5
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2023-11-15 11:56:50 +0900

    絶妙に7:3にならない

--- app/src/main/java/com/example/utakata_medicine/MainActivity.kt
+++ app/src/main/java/com/example/utakata_medicine/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+//import androidx.compose.foundation.layout.RowScopeInstance.align
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -29,6 +31,7 @@ import com.example.utakata_medicine.ui.theme.UtakatamedicineTheme
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
@@ -42,29 +45,13 @@ class MainActivity : ComponentActivity() {
             UtakatamedicineTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().fillMaxHeight(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     UtakataUnderTabLayout()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UtakatamedicineTheme {
-        Greeting("Android")
     }
 }
 
@@ -80,9 +67,12 @@ data class Medicine (
 @Composable
 fun UtakataMedicineTable(data: List<Medicine>){
     UtakatamedicineTheme {
-        Scaffold(containerColor = MaterialTheme.colorScheme.surface) {
-                padding -> List(data.size) { index -> index to "Item $index"
-        }
+        Scaffold(
+            containerColor = MaterialTheme.colorScheme.surface,
+            modifier = Modifier.fillMaxHeight(0.7F)
+        )
+        {
+            padding -> List(data.size) { index -> index to "Item $index" }
             val column1Weight = .3f
             val column2Weight = .7f
 
@@ -101,7 +91,7 @@ fun UtakataMedicineTable(data: List<Medicine>){
 
                 data.map {
                     Row(
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier.fillMaxSize()
                     ) {
                         TableCell(text = it.name, weight = column1Weight)
                         TableCell(text = it.piecestr, weight = column2Weight)
@@ -134,23 +124,38 @@ fun UtakataUnderTabLayout(){
     val medicineData:List<Medicine> = listOf(
         testData,testData2,testData.copy(),testData2.copy()
         )
+    // val UnitList = listOf(UtakataMedicineTable(data = medicineData),Text(text = "test2"),Text(text = "test3"))
     UtakatamedicineTheme {
-        Column {
-            when (selectedItem) {
-                0 -> UtakataMedicineTable(data = medicineData)
-                1 -> Text("1")
-                2 -> Text("2")
-            }
-            NavigationBar {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        selected = selectedItem == index,
-                        onClick = { selectedItem = index },
-                        label = { Text(text = item) },
-                        icon = { Icon(Icons.Filled.Favorite, item) }
-                    )
+            Column(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight()
+            ) {
+                Row(
+                    //modifier = Modifier.fillMaxWidth(0.7f).height(LocalConfiguration.current.screenHeightDp.dp / 3)
+                )
+                {
+                    when (selectedItem) {
+                        0 -> UtakataMedicineTable(data = medicineData)
+                        1 -> Text(text = "test2",modifier = Modifier.fillMaxHeight(0.7f))
+                        2 -> Text(text = "test3",modifier = Modifier.fillMaxHeight(0.7f))
+                    }
+                }
+                Row {
+                    NavigationBar(
+                        modifier = Modifier
+                            .fillMaxWidth().fillMaxHeight(0.3f)//LocalConfiguration.current.screenHeightDp.dp / 7)
+                    ) {
+                        items.forEachIndexed { index, item ->
+                            NavigationBarItem(
+                                selected = selectedItem == index,
+                                onClick = { selectedItem = index },
+                                label = { Text(text = item) },
+                                icon = { Icon(Icons.Filled.Favorite, item) }
+                            )
+                        }
+                    }
                 }
             }
-        }
+
+
     }
 }
